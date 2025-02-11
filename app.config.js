aade9ed4f1b43338d6559e8e766c99123e5639dc
Gerrod Schirtzinger <149504119+GerrodSchirtzingerInsight@users.noreply.github.com>
2025-02-11 10:30:07 -0500

    tutorial changes

---
+++ app.config.js
@@ -0,0 +1,88 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.grodthehippy.stickersmash.dev";
+  }
+
+  if (IS_PREVIEW) {
+    return "com.grodthehippy.stickersmash.preview";
+  }
+
+  return "com.grodthehippy.stickersmash";
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "StickerSmash (Dev)";
+  }
+
+  if (IS_PREVIEW) {
+    return "StickerSmash (Preview)";
+  }
+
+  return "StickerSmash: Emoji Stickers";
+};
+
+export default {
+  expo: {
+    name: getAppName(),
+    slug: "StickerSmash",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: getUniqueIdentifier(),
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: getUniqueIdentifier(),
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "a7f88d3e-b0a6-4876-af52-4c68dfffbc4d",
+      },
+    },
+    owner: "grodthehippy",
+    updates: {
+      url: "https://u.expo.dev/a7f88d3e-b0a6-4876-af52-4c68dfffbc4d",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+  },
+};
